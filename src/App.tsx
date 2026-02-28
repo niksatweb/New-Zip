a5dff56812f209ecb943f0c33cc08b676e7365a1
Nick <niksatweb@gmail.com>
2026-02-28 20:20:58 +0300

    Add daisyui

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,10 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1 className="underline">
-        Hello world!
+      <h1 className="underline text-3xl">
+        Hello!
       </h1>
+      <button className="btn btn-primary">Test Button</button>
     </>
   );
 }
