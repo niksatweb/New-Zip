074d685453d7898970359543c050db8223d75ff5
Nick <niksatweb@gmail.com>
2026-03-03 12:46:18 +0100

    add-router refactor: removed conflict in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,28 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router/dom";
-import {Router} from './app/Router.ts'
-
-<<<<<<< Updated upstream
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <div>This is the text from the router element</div>,
-  },
-]);
-=======
->>>>>>> Stashed changes
+import { Router } from "./app/Router.ts";
 
 const root = document.getElementById("root");
 
 createRoot(root!).render(
   <StrictMode>
-<<<<<<< Updated upstream
-   <RouterProvider router={router}>
-     </RouterProvider> 
-=======
-    <RouterProvider router={Router}>
-    </RouterProvider>
->>>>>>> Stashed changes
+    <RouterProvider router={Router}></RouterProvider>
   </StrictMode>,
 );
