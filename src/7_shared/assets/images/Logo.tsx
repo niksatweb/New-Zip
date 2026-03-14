da02c4b813029ea120ab7608f724037a85d9e349
Nick <niksatweb@gmail.com>
2026-03-15 00:43:14 +0300

    13-add-topbar feat: make Logo bigger

--- src/7_shared/assets/images/Logo.tsx
+++ src/7_shared/assets/images/Logo.tsx
@@ -1,5 +1,7 @@
-export const Logo = ({className}:{className: string}) => {
-  return (
-    <div className={className}>New-Zip</div>
-  )
+export const Logo = ({ className, href="#" }: { className: string, href?: string }) => {
+    return (
+        <a href={href}>
+            <div className={className}>New-Zip</div>
+        </a>
+    )
 }
