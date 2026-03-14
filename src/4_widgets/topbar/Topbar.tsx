a1800d487c8fd92a809c42effc0c381fa86cf606
Nick <niksatweb@gmail.com>
2026-03-14 20:27:42 +0300

    13-add-topbar feat: add logo

--- src/4_widgets/topbar/Topbar.tsx
+++ src/4_widgets/topbar/Topbar.tsx
@@ -1,3 +1,8 @@
+import { Logo } from "@/7_shared/assets/images/Logo"
+
+
 export function Topbar() {
-  return <div></div>
+  return <div className="bg-gray-300 w-full xl:w-10/12 h-15 flex justify-between items-center">
+    <Logo className="uppercase font-bold text-3xl"/>
+    </div>
 }
---
+++ src/7_shared/assets/images/Logo.tsx
@@ -0,0 +1,5 @@
+export const Logo = ({className}:{className: string}) => {
+  return (
+    <div className={className}>New-Zip</div>
+  )
+}
