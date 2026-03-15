10e570bbb6e2ef89411f7ad868b49b5c2ec4b99b
Nick <niksatweb@gmail.com>
2026-03-15 22:29:54 +0300

    13-add-topbar WIP

--- src/4_widgets/topbar/Topbar.tsx
+++ src/4_widgets/topbar/Topbar.tsx
@@ -2,8 +2,12 @@ import { Logo } from "@/7_shared/assets/images/Logo"
 import { HamburgerButton } from "@/7_shared/ui/button/HamburgerButton"
 import { Navbar } from "../navbar"
 import { AskQuestionButton } from "@/7_shared/ui/button/AskQuestionButton"
+import { useState } from "react"
 
 export function Topbar() {
+  const [mobMenuIsOpen, setMobMenuOpen] = useState(false);
+  const handleClick = () => setMobMenuOpen(!mobMenuIsOpen)
+
   return <div className="container mx-auto max-w-354 px-1 h-12 flex justify-between items-center">
     <Logo className="uppercase font-bold text-4xl whitespace-nowrap" />
     <div className="items-center justify-between gap-x-4 hidden md:flex">
@@ -11,7 +15,10 @@ export function Topbar() {
       <AskQuestionButton className="bg-amber-400 hover:bg-amber-300 px-4 py-1 rounded-2xl whitespace-nowrap"></AskQuestionButton>
     </div>
     <div className="flex md:hidden" >
-      <HamburgerButton href="#" />
+      <HamburgerButton onClick={handleClick} href="#" />
+    </div>
+    <div className="absolute right-0 top-12 z-10 bg-amber-50">
+      {mobMenuIsOpen && <Navbar />}
     </div>
 
 
