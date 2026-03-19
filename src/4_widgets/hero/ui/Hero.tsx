f45f8581c72618d86ef55844e823f7d61deaa37f
Nick <niksatweb@gmail.com>
2026-03-19 19:18:14 +0300

    15-add-hero: WIP

--- src/4_widgets/hero/ui/Hero.tsx
+++ src/4_widgets/hero/ui/Hero.tsx
@@ -2,27 +2,12 @@ import { Container } from "@/7_shared/ui";
 
 export const Hero = ({ className }: { className?: string }) => {
   return (
-<<<<<<< HEAD
+
     <Container className={"relative text-primary " + (className ?? "")}>
       <div className="w-full bg-primary text-primary-content flex h-20 items-center">
         <h1
           className={"ml-5 text-primary text-2xl"}
-        >
-          Поставки запчастей любого <br/> мирового бренда
-=======
-    <Container className="text-primary">
-      <div className="relative text-primary-content h-50">
-        <h1
-          className="
-        px-1 top-6 
-        max-w-100 text-2xl
-         absolute
-        md:text-3xl
-        text-primary"
-        >
-          Поставки запчастей любого мирового бренда
->>>>>>> 56fae87 (15-add-hero: WIP)
-        </h1>
+        >Поставки запчастей любого <br /> мирового бренда</h1>
       </div>
     </Container>
   );
