56f52eb3c69ad4517b0ba92ef9298fe6e65baecd
Nick <niksatweb@gmail.com>
2026-03-21 22:45:24 +0300

    13-add-topbar: WIP

--- src/4_widgets/topbar/ui/Topbar.tsx
+++ src/4_widgets/topbar/ui/Topbar.tsx
@@ -3,24 +3,16 @@ import { HamburgerButton } from "@/7_shared/ui/buttons/HamburgerButton"
 import { Navbar } from "../../navbar"
 import { AskQuestionButton } from "@/7_shared/ui/buttons/AskQuestionButton"
 import { useState } from "react"
-<<<<<<< Updated upstream
-import { SearchInput } from "@/5_features/search"
-=======
 import { Container } from "@/7_shared/ui"
 import { SearchInput } from "@/5_features/search"
 
->>>>>>> Stashed changes
 
 export function Topbar({ className }: { className: string }) {
   const [mobMenuIsOpen, setMobMenuOpen] = useState(false);
   const handleClick = () => setMobMenuOpen(!mobMenuIsOpen)
 
   return (
-<<<<<<< Updated upstream
-    <div className={"container max-w-354 px-1 h-15 flex justify-between items-center gap-x-5 " + (className ?? '')}>
-=======
     <Container className={"h-12 flex gap-x-10 " + (className ?? "")}>
->>>>>>> Stashed changes
       <Logo className="uppercase font-bold text-4xl whitespace-nowrap" />
       <SearchInput className="flex-1"></SearchInput>
       <div className="items-center md:justify-between gap-x-4 flex">
