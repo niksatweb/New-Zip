72b0b9f83fc18fb6a64d84f2e3162eda71c0f914
Nick <niksatweb@gmail.com>
2026-03-26 21:30:36 +0300

    19-add-product-entity: correct according to PR comments

--- src/4_entities/product/model/types/product.backend.ts
+++ src/4_entities/product/model/types/product.backend.ts
@@ -1,18 +1,21 @@
+export const ProductCondition = {
+  New: "NEW",
+  Used: "USED",
+} as const
+
 export interface ProductBackend {
-  condition: "Новый" | "Б/У"
-  inFavorites: boolean
+  condition: (typeof ProductCondition)[keyof typeof ProductCondition]
   previewImageUrl: string
   previewImageAlt: string
   brand: string
   name: string
   deliveryTime: number
   price: number
-  inBasket: boolean
   className: string
 }
 
 export interface ProductDetailBackend extends ProductBackend {
-  onStock: "В наличии" | "Под заказ"
+  onStock: boolean
   description: string
   images: string[]
   manufacturerId: string
