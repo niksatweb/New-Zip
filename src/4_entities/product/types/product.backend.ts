b50e1631cc244d383d48331ddb126b77570dec6a
Nick <niksatweb@gmail.com>
2026-03-26 21:46:18 +0300

    19-add-product-entity: remove className from interface and change condition to New or Used

--- src/4_entities/product/types/product.backend.ts
+++ src/4_entities/product/types/product.backend.ts
@@ -1,17 +1,11 @@
-export const ProductCondition = {
-  New: "NEW",
-  Used: "USED",
-} as const
-
 export interface ProductBackend {
-  condition: (typeof ProductCondition)[keyof typeof ProductCondition]
+  condition: "New" | "Used"
   previewImageUrl: string
   previewImageAlt: string
   brand: string
   name: string
   deliveryTime: number
   price: number
-  className: string
 }
 
 export interface ProductDetailBackend extends ProductBackend {
