487771fb1e11a2777f779c87f0e8214508bb155b
Nick <niksatweb@gmail.com>
2026-03-30 23:51:06 +0300

    22-add-sidebar: WIP

--- src/2_pages/home/Home.tsx
+++ src/2_pages/home/Home.tsx
@@ -6,7 +6,7 @@ export function Home() {
   return (
     <Container className="mx-auto flex justify-center items-center flex-col">
       <main className="flex gap-x-16 justify-between w-full">
-        <Sidebar className="p-6 w-64 pl-0">
+        <Sidebar className="px-6 w-64 pl-0">
         </Sidebar>
         <section className="flex-1">
           <p>section part</p>
