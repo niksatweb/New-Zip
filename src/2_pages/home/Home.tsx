e9e5c6890639d30021b9c6e79c88e954dc425ad9
Nick <niksatweb@gmail.com>
2026-04-01 19:47:30 +0300

    add page-title and page-subtitle

--- src/2_pages/home/Home.tsx
+++ src/2_pages/home/Home.tsx
@@ -1,14 +1,17 @@
-import { Container } from "@/5_shared/ui"
+import { Container, PageTitle, PageSubtitle } from "@/5_shared/ui"
 import { Sidebar } from "@/3_widgets/sidebar/"
-import { Link } from "react-router"
 
 export function Home() {
   return (
     <Container className="mx-auto flex justify-center items-center flex-col">
       <main className="flex gap-x-16 justify-between w-full">
-        <Sidebar className="px-6 w-64 pl-0">
-        </Sidebar>
-        <section className="flex-1">
+        <Sidebar className="px-6 w-64 pl-0"></Sidebar>
+        <section className="flex-1 py-6">
+          <PageTitle>Промышленное оборудование</PageTitle>
+          <PageSubtitle>
+            Прямые поставки от ведущих мировых производителей. Официальная
+            гарантия и техническая поддержка.
+          </PageSubtitle>
           <p>section part</p>
         </section>
       </main>
