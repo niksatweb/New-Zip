0534dddc1605cd9c822c6155cf25fad417b1c700
Arsen <keetrussia@mail.ru>
2026-04-06 18:22:12 +0300

    26-product refactor: home add new api

--- src/1_app/main.tsx
+++ src/1_app/main.tsx
@@ -3,11 +3,15 @@ import { createRoot } from "react-dom/client"
 import "./styles/globals.css"
 import { RouterProvider } from "react-router/dom"
 import { Router } from "@/1_app/router"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 
 const root = document.getElementById("root")
+const queryClient = new QueryClient()
 
 createRoot(root!).render(
   <StrictMode>
-    <RouterProvider router={Router}></RouterProvider>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={Router}></RouterProvider>
+    </QueryClientProvider>
   </StrictMode>
 )
