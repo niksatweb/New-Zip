3de40e9b4185440c98ebd9367835361806cce562
Nick <niksatweb@gmail.com>
2026-04-14 20:33:03 +0300

    28-add-product-detail-page: WIP

--- src/2_pages/product-detail-page/ProductDetailPage.tsx
+++ src/2_pages/product-detail-page/ProductDetailPage.tsx
@@ -12,36 +12,45 @@ import { ProductCondition } from "@/4_entities/product/ui/productCard/ProductCon
 import { ImageGallery } from "@/3_widgets/imageGallery"
 import { ProductDetailGallery } from "@/4_entities/product"
 import { Loader2 } from "lucide-react"
+import { Download } from "lucide-react"
 
 export const ProductDetailPage = () => {
   const { id } = useParams()
   const { productDetail, isLoading } = useProductDetailQuery(id)
-  return (
+  return isLoading ? (
+    <div className="w-full h-150 flex items-center justify-center">
+      <Loader2 size={160} className="animate-spin text-primary/20" />
+    </div>
+  ) : (
     <Container className="mx-auto flex justify-center items-center h-full">
-      {isLoading ? (
-        <Loader2 size={60} className="animate-spin" />
-      ) : (
-        <div className="flex items-start justify-center w-full gap-x-20">
-          <div className="">
-            <ProductDetailGallery
-              className="max-w-120"
-              product={productDetail}
-            ></ProductDetailGallery>
-          </div>
-          <div className="flex-1 flex flex-col gap-y-4">
-            <ProductName name={productDetail?.name}></ProductName>
-            <ProductCondition className="max-w-10"
-              condition={productDetail?.condition}
-            ></ProductCondition>
-            <p>{productDetail?.description}</p>
-            <ProductPrice
-              className="flex items-end gap-x-8 text-xl"
-              price={productDetail?.price}
-            ></ProductPrice>
-            <p>Скачать техническую документацию: </p>
-          </div>
+      <div className="flex items-start justify-center w-full gap-x-20">
+        <div className="">
+          <ProductDetailGallery
+            className="max-w-120"
+            product={productDetail}
+          ></ProductDetailGallery>
         </div>
-      )}
+        <div className="flex-1 flex flex-col gap-y-4">
+          <ProductName name={productDetail?.name}></ProductName>
+          <ProductCondition
+            className="max-w-20"
+            condition={productDetail?.condition}
+          ></ProductCondition>
+          <p>{productDetail?.description}</p>
+          <ProductPrice
+            className="flex items-end gap-x-8 text-xl"
+            price={productDetail?.price}
+          ></ProductPrice>
+
+          <p className="flex">
+            Техническая документация:
+            <a href={productDetail?.datasheet} className="flex gap-x-2">
+              Скачать
+              <Download />
+            </a>
+          </p>
+        </div>
+      </div>
     </Container>
   )
 }
