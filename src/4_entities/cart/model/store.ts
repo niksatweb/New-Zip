8be9b8bef9065f1f3d022ecc4158db6a2fd85868
Nick <niksatweb@gmail.com>
2026-05-30 16:33:03 +0700

    30-add-cart-entity: remove checking for existing in cart items

--- src/4_entities/cart/model/store.ts
+++ src/4_entities/cart/model/store.ts
@@ -15,33 +15,23 @@ interface cartState {
   removeAllProducts: () => void
 }
 
-export const useCartStore = create<cartState>((set, get) => ({
-  cartItems: [],
-  addProduct: (product: ProductDetail) =>
-    set((state: cartState) => {
-      const existingItem = state.cartItems.find(
-        (item) => item.id === product.id
-      )
-
-      if (existingItem) {
-        // Товар уже в корзине → увеличиваем количество
-        return {
-          cartItems: state.cartItems.map((item) =>
-            item.id === product.id
-              ? { ...item, quantity: item.quantity + 1 }
-              : item
-          ),
-        }
-      }
-
-      // Нового товара нет → добавляем с quantity = 1
-      return {
-        cartItems: [...state.cartItems, { ...product, quantity: 1 }],
-      }
+export const useCartStore = create<cartState>()(
+  persist(
+    (set, get) => ({
+      cartItems: [],
+      addProduct: (product: ProductDetail) =>
+        set((state: cartState) => {
+          // Нового товара нет → добавляем с quantity = 1
+          return {
+            cartItems: [...state.cartItems, { ...product, quantity: 1 }],
+          }
+        }),
+      removeItem: (id: ProductBackend["id"]) =>
+        set((state) => ({
+          cartItems: state.cartItems.filter((i: CartItem) => i.id !== id),
+        })),
+      removeAllProducts: () => set({ cartItems: [] }),
     }),
-  removeItem: (id: ProductBackend["id"]) =>
-    set((state) => ({
-      cartItems: state.cartItems.filter((i: CartItem) => i.id !== id),
-    })),
-  removeAllProducts: () => set({ cartItems: [] }),
-}))
+    { name: "cartItem" }
+  )
+)
